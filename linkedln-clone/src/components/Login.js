ca9860bdbafbb085a01a853e181a639b99664bc1
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-06-19 21:27:21 +0530

    made responsive

--- linkedln-clone/src/components/Login.js
+++ linkedln-clone/src/components/Login.js
@@ -14,6 +14,7 @@ const Login = (props) => {
       <Section>
         <Hero>
           <h1>Welcome to your professional community</h1>
+          <img src="/images/login-hero.svg" alt="" />
         </Hero>
       </Section>
     </Container>
@@ -100,6 +101,22 @@ const Hero = styled.div`
     @media (max-width: 768px) {
       text-align: center;
       font-size: 20px;
+      width: 100%;
+      line-height: 2;
+    }
+  }
+  img {
+    /* z-index: -1; */
+    width: 700px;
+    height: 670px;
+    position: absolute;
+    bottom: -2px;
+    right: -150px;
+    @media (max-width: 768px) {
+      top: 230px;
+      width: initial;
+      position: initial;
+      height: initial;
     }
   }
 `;
