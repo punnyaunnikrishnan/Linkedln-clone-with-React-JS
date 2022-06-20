aad23197a843355deae6309691a4f478f0a9d40d
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-06-20 18:10:47 +0530

    home route added

--- linkedln-clone/src/App.js
+++ linkedln-clone/src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
 import Login from "./components/Login";
-
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Login />} />
+          <Route exact path="/home" element={<Header />} />
         </Routes>
       </BrowserRouter>
     </div>
