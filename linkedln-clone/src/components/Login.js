b1ee7a70ff3babd426afc304eff891580ba8ae9d
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-06-20 17:49:53 +0530

    google button styling completed

--- linkedln-clone/src/components/Login.js
+++ linkedln-clone/src/components/Login.js
@@ -147,6 +147,11 @@ const Google = styled.button`
   z-index: 0;
   transition-duration: 167ms;
   font-size: 20px;
+  color:rgba(0,0,0,0.6);
+  &:hover{
+    background-color:rgba(207,207,207,0.25);
+    color:rgba(0,0,0,0.75);
+  }
 `;
 
 export default Login;
