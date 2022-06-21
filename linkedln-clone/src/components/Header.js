b3284eaa6984063d3e07cc1a2b22ef162c2ffd9d
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-06-21 12:00:22 +0530

    Header.js

--- linkedln-clone/src/components/Header.js
+++ linkedln-clone/src/components/Header.js
@@ -8,6 +8,14 @@ const Header = (props) => {
             <img src="/images/home-logo.svg" alt="" />
           </a>
         </Logo>
+        <Search>
+          <div>
+            <input type="text" placeholder="Search" />
+          </div>
+          <SearchIcon>
+            <img src="/images/search-icon.svg" alt="" />
+          </SearchIcon>
+        </Search>
       </Content>
     </Container>
   );
@@ -34,4 +42,28 @@ const Logo = styled.span`
   margin-right: 8px;
   font-size: 0 px;
 `;
+const Search = styled.div`
+  opacity: 1;
+  flex-grow: 1;
+  position: relative;
+  & > div {
+    max-width: 280px;
+    input {
+      border: none;
+      box-shadow: none;
+      background-color: #eef3f8;
+      border-radius: 2px;
+      color: rgba(0, 0, 0, 0.9);
+      width: 218px;
+      padding: 0 8px 0 40px;
+      line-height: 1.75;
+      font-weight: 400;
+      font-size: 14px;
+      height: 34px;
+      border-color: #dce6f1;
+      vertical-align: text-top;
+    }
+  }
+`;
+const SearchIcon = styled.div``;
 export default Header;
