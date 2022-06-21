b565b36dc7b5bfa448029955ca19578875df1b7d
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-06-21 11:39:08 +0530

    added logo

--- linkedln-clone/src/components/Header.js
+++ linkedln-clone/src/components/Header.js
@@ -1,5 +1,37 @@
 import styled from "styled-components";
 const Header = (props) => {
-  return <div>Header</div>;
+  return (
+    <Container>
+      <Content>
+        <Logo>
+          <a href="/home">
+            <img src="/images/home-logo.svg" alt="" />
+          </a>
+        </Logo>
+      </Content>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  background-color: white;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.8);
+  left: 0;
+  padding: 0 24px;
+  position: fixed;
+  top: 0;
+  width: 100vw;
+  z-index: 100;
+`;
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+  min-height: 100%;
+  max-width: 1128px;
+`;
+const Logo = styled.span`
+  margin-right: 8px;
+  font-size: 0 px;
+`;
 export default Header;
