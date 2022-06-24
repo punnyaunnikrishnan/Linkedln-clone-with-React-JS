5a60d96555c48e906268cdee871e653693c17717
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-06-24 21:49:51 +0530

    Leftside modified

--- linkedln-clone/src/components/Leftside.js
+++ linkedln-clone/src/components/Leftside.js
@@ -3,4 +3,7 @@ const leftSide = (props) => {
   return;
   <Container>Leftside</Container>;
 };
-const Container=styled.div``;
+const Container = styled.div`
+  grid-area: leftside;
+`;
+export default leftSide;
