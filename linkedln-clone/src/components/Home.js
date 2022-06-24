9073f71d401cf285731cf38db53053ca07d5e519
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-06-24 21:00:32 +0530

    added container and section for hiring portion

--- linkedln-clone/src/components/Home.js
+++ linkedln-clone/src/components/Home.js
@@ -1,5 +1,21 @@
 import styled from "styled-components";
 const Home = (props) => {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <Section>
+        <h5>
+          <a>Hiring in a hurry? -</a>
+        </h5>
+        <p>
+          Find talented pros in record time with Upwork and keep business
+          moving.
+        </p>
+      </Section>
+    </Container>
+  );
 };
+const Container=styled.div``;
+const Content=styled.div``;
+const Section=styled.section``;
+
 export default Home;
