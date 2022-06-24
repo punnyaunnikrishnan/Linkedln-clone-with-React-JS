e73157a4746cdb1a8de447ccb1e85be31e5b7f3f
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-06-24 21:50:17 +0530

    Rightside modified

---
+++ linkedln-clone/src/components/Rightside.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+const Rightside = (props) => {
+  return;
+  <Container>Rightside</Container>;
+};
+const Container = styled.div`
+  grid-area: Rightside;
+`;
+export default RightSide;
