679f50d494ad55bab4890afdb3fce73b7ea70069
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-06-28 20:59:50 +0530

    Leftside.js

--- linkedln-clone/src/components/Leftside.js
+++ linkedln-clone/src/components/Leftside.js
@@ -25,6 +25,7 @@ const Leftside = (props) => {
         <Item>
           <span>
             <img src="/images/item-icon.svg" alt="" />
+            My Items
           </span>
         </Item>
       </ArtCard>
