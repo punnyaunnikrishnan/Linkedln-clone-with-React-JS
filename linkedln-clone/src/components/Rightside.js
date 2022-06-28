07fadfaaa6aa03031dedddc5c76d55dd46962327
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-06-28 22:20:18 +0530

    followcard added and styled

--- linkedln-clone/src/components/Rightside.js
+++ linkedln-clone/src/components/Rightside.js
@@ -1,9 +1,24 @@
 import styled from "styled-components";
 const Rightside = (props) => {
-  return;
-  <Container>Rightside</Container>;
+  return (
+    <Container>
+      <FollowCard>Rightside</FollowCard>
+    </Container>
+  );
 };
 const Container = styled.div`
   grid-area: rightside;
 `;
+const FollowCard = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 20%);
+  padding: 12px;
+`;
+
 export default Rightside;
