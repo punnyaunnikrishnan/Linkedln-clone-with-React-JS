3b98037af5183f63b6f1aa6e07e1d1bf93abfca2
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-06-30 21:37:00 +0530

    bannercard img added

--- linkedln-clone/src/components/Rightside.js
+++ linkedln-clone/src/components/Rightside.js
@@ -32,7 +32,12 @@ const Rightside = (props) => {
           <img src="images/right-icon.svg" alt="" />
         </Recommendation>
       </FollowCard>
-      <BannerCard></BannerCard>
+      <BannerCard>
+        <img
+          src="https://static-exp1.licdn.com/scds/common/u/images/promo/ads/li_evergreen_jobs_ad_300x250_v1.jpg"
+          alt=""
+        />
+      </BannerCard>
     </Container>
   );
 };
