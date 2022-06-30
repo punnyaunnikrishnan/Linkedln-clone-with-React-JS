149b03537947e179ad8253178cbd1bccc550a73d
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-06-30 21:55:05 +0530

    sharebox component added

--- linkedln-clone/src/components/Main.js
+++ linkedln-clone/src/components/Main.js
@@ -1,9 +1,27 @@
 import styled from "styled-components";
 const Main = (props) => {
-  return;
-  <Container>Main</Container>;
+  return (
+    <Container>
+      <ShareBox>Share</ShareBox>
+    </Container>
+  );
 };
 const Container = styled.div`
   grid-area: main;
+`;
+const CommonCard = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  box-shadow: 0 0 0 1px rgb(0 0 0 /151), 0 0 0 rgb(0 0 0 /201);
+`;
+
+const ShareBox = styled(CommonCard)`
+  display: flex;
+  flex-direction: column;
 `;
 export default Main;
